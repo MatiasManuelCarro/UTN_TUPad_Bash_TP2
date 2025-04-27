5a68e6e2c6becd95b22dbd7e79ff6ac9731c168d
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-04-27 15:49:04 -0300

    ejercicios 2

---
+++ ejercicio2_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear -x
+
+echo "Ingrese base y altura de un triangulo para clacular su area"
+read -p "Ingrese la base: " base
+read -p "Ingrese la altura: " altura
+
+area=$(echo "scale=2; $base * $altura" | bc)
+echo "El area del rectangulo es $area"
---
+++ ejercicio2_3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear -x
+read -p "Ingrese su nombre: " nombre
+read -p "Ingrese su edad: " edad
+read -p "Ingrese ciudad: " ciudad
+
+printf "Hola, soy $nombre, tengo $edad años y soy de $ciudad"
